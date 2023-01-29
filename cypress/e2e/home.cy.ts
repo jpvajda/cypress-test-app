6a15d9656992a8612ad7900f965929de8f4e6439
John P. Vajda <john.vajda@gmail.com>
2023-01-29 15:00:30 -0700

    added new tests

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -7,7 +7,7 @@ describe("home page", () => {
   context( "Hero section", () => {
 
     it("the h1 contains the correct text", () => {
-      cy.get("[data-test='hero-heading']")
+      cy.getByData("hero-heading")
       .contains("Testing Next.js Applications with Cypress")
     })
 
@@ -18,6 +18,23 @@ describe("home page", () => {
     })
   })
 
+  context("Course section", () => {
+    it.only("Course: Testing Your First Next.js Application", () => {
+      cy.getByData("course-0").find("a").eq(3).click()
+      cy.location("pathname").should("eq", "/testing-your-first-application")
+    })
+    it.only("Course: Testing Foundations", () => {
+      cy.getByData("course-1").find("a").eq(3).click()
+      cy.location("pathname").should("eq", "/testing-foundations")
+    })
+    it.only("Course: Cypress Fundamentals", () => {
+      cy.getByData("course-2").find("a").eq(3).click()
+      cy.location("pathname").should("eq", "/cypress-fundamentals")
+    })
+
+  })
+
 })
+
 
 
